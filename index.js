c752d1bfcf254c34b639206549eaf751f0d61118
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-12-07 23:15:08 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// #7kyu Vowel Count
+// function getCount(str) {
+//   const vowel = "aeiou";
+//   let count = 0;
+//   for (const i of str) {
+//     if (vowel.includes(i)) count++;
+//   }
+//   return count;
+// }
+
+// #6kyu Who likes it?
+// function likes(names) {
+//   if (names.length == 1) {
+//     return `${names} likes this`;
+//   } else if (names.length == 2) {
+//     return `${names[0]} and ${names[1]} like this`;
+//   } else if (names.length == 3) {
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//   } else if (names.length > 3) {
+//     return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+//   } else {
+//     return "no one likes this";
+//   }
+// }
