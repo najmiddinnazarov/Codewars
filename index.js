b375948a58376efb03448ba3f460f52a58c963b6
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-12-08 19:36:17 +0500

    Add several issues

--- index.js
+++ index.js
@@ -33,3 +33,92 @@
 //   return Object.entries(obj);
 // };
 // console.log(orderedCount("233312"));
+
+// #7kyu Squares sequence
+// function squares(x, n) {
+//   let result = [];
+//   n > 0 && n !== undefined ? result.push(x) : [];
+//   for (let i = 1; i < n; i++) {
+//     result.push((x = x ** 2));
+//   }
+//   return result;
+// }
+// console.log(squares(2, -1));
+
+// #8kyu Get Nth Even Number
+// const nthEven = (n) => n * 2 - 2;
+// console.log(nthEven(3));
+
+// #7kyu shorter concat [reverse longer]
+// function shorter_reverse_longer(a, b) {
+//   let reverseLetterA = a.split("").reverse().join("");
+//   let reverseLetterB = b.split("").reverse().join("");
+//   if (a.length >= b.length) {
+//     return `${b}${reverseLetterA}${b}`;
+//   } else {
+//     return `${a}${reverseLetterB}${a}`;
+//   }
+// }
+// console.log(shorter_reverse_longer("fghi", "abcde"));
+
+// #7kyu Find Count of Most Frequent Item in an Array
+// function mostFrequentItemCount(collection) {
+//   let res = [];
+//   let obj = {};
+//   if (collection.length !== 0) {
+//     for (const i of collection) {
+//       if (i in obj) obj[i]++;
+//       else obj[i] = 1;
+//     }
+//   } else return 0;
+//   for (const key in obj) {
+//     res.push(obj[key]);
+//   }
+//   return Math.max(...res);
+// }
+// console.log(mostFrequentItemCount([]));
+
+// #6kyu Convert string to camel case
+// function toCamelCase(str) {
+//   let arr = str.split("");
+//   for (const i of arr) {
+//     if (i == "_") {
+//       let index = arr.indexOf("_");
+//       arr.splice(index, 2, arr[index + 1].toUpperCase());
+//     } else if (i == "-") {
+//       let index = arr.indexOf("-");
+//       arr.splice(index, 2, arr[index + 1].toUpperCase());
+//     }
+//   }
+//   return arr.join("");
+// }
+// console.log(toCamelCase("A-B-C"));
+
+// #7kyu Debug Sum of Digits of a Number
+// function getSumOfDigits(integer) {
+//   var sum = [];
+//   var digits = Math.trunc(integer).toString();
+//   for (var ix = 0; ix < digits.length; ix++) {
+//     sum.push(digits[ix]);
+//   }
+//   return eval(sum.join("+"));
+// }
+// console.log(getSumOfDigits(223));
+
+// 7kyu V A P O R C O D E
+// function vaporcode(string) {
+//   let str = string.split(" ").join("").toUpperCase();
+//   return str.split("").join("  ");
+// }
+// console.log(vaporcode("Why isnt my code working"));
+
+// #6kyu Bit Counting
+// var countBits = function (n) {
+//   let binary = n.toString(2);
+//   let count = 0;
+//   for (const i of binary) {
+//     if (i > 0) count++;
+//   }
+//   return count;
+// };
+// console.log(countBits(1234));
