1f022bc4877ddeeecfcd4bbd7cd181501aaaa330
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-02-12 18:29:48 +0500

    #7kyu Chinese Zodiac

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -2602,3 +2602,28 @@ console.log(shortForm("asault")); */
   return reverse;
 }
 console.log(flipNumber("012345")); */
+
+// #7kyu Chinese Zodiac
+/* function chineseZodiac(year) {
+  const animals = [
+    "Rat",
+    "Ox",
+    "Tiger",
+    "Rabbit",
+    "Dragon",
+    "Snake",
+    "Horse",
+    "Goat",
+    "Monkey",
+    "Rooster",
+    "Dog",
+    "Pig",
+  ];
+  const elements = ["Wood", "Fire", "Earth", "Metal", "Water"];
+  const baseYear = 1924;
+  const givenYear = year - baseYear;
+  const element = elements[Math.floor(givenYear / 2) % 5];
+  const animal = animals[givenYear % 12];
+  return element + " " + animal;
+}
+console.log(chineseZodiac(1965)); */
