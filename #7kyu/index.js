bc4e6a30220f2bdce474c1b0bf978792a681065c
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-03-04 21:53:01 +0500

    #7kyu Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -2878,3 +2878,53 @@ console.log(
     },
   ])
 ); */
+
+// #7kyu Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins
+/* function findAdmin(list, lang) {
+  return list.filter(
+    (value) => value.language === lang && value.githubAdmin === "yes"
+  );
+}
+console.log(
+  findAdmin(
+    [
+      {
+        firstName: "Harry",
+        lastName: "K.",
+        country: "Brazil",
+        continent: "Americas",
+        age: 22,
+        language: "JavaScript",
+        githubAdmin: "yes",
+      },
+      {
+        firstName: "Kseniya",
+        lastName: "T.",
+        country: "Belarus",
+        continent: "Europe",
+        age: 49,
+        language: "Ruby",
+        githubAdmin: "no",
+      },
+      {
+        firstName: "Jing",
+        lastName: "X.",
+        country: "China",
+        continent: "Asia",
+        age: 34,
+        language: "JavaScript",
+        githubAdmin: "yes",
+      },
+      {
+        firstName: "Piotr",
+        lastName: "B.",
+        country: "Poland",
+        continent: "Europe",
+        age: 128,
+        language: "JavaScript",
+        githubAdmin: "no",
+      },
+    ],
+    "JavaScript"
+  )
+); */
