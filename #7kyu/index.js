8f54282ddb8e5ccd1f4628a76099eebf57893d50
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-04-04 06:04:01 +0500

    function toLeetSpeak(str) {

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -3211,3 +3211,43 @@ console.log(frogContest(5)); */
   return array;
 }
 console.log(divisibleByLast(73312)); */
+
+// #7kyu ToLeetSpeak
+/* function toLeetSpeak(str) {
+  let string = "";
+  const alphabet = {
+    A: "@",
+    B: "8",
+    C: "(",
+    D: "D",
+    E: "3",
+    F: "F",
+    G: "6",
+    H: "#",
+    I: "!",
+    J: "J",
+    K: "K",
+    L: "1",
+    M: "M",
+    N: "N",
+    O: "0",
+    P: "P",
+    Q: "Q",
+    R: "R",
+    S: "$",
+    T: "7",
+    U: "U",
+    V: "V",
+    W: "W",
+    X: "X",
+    Y: "Y",
+    Z: "2",
+    " ": " ",
+  };
+  for (let i = 0; i < str.length; i++) {
+    string += alphabet[str[i]];
+  }
+  return string;
+}
+console.log(toLeetSpeak("CODE WARS"));
+ */
