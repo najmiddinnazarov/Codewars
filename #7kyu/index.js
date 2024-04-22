af835ab9d927c6b2e09171deb42e92ac1ed39151
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-04-22 06:20:30 +0500

    #7kyu Make them bark!

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -3427,3 +3427,16 @@ console.log(gen.next().value); */
   return sum >= 0 ? sum : 0;
 }
 console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"])); */
+
+// #7kyu Make them bark!
+/* function Dog(name, breed, sex, age) {
+  this.name = name;
+  this.breed = breed;
+  this.sex = sex;
+  this.age = age;
+}
+Dog.prototype.bark = function () {
+  return "Woof!";
+};
+var apollo = new Dog("Apollo", "Dobermann", "male", "4");
+console.log(apollo.bark()); */
