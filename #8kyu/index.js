a329527c9f18a63062c5345b6935435bdc307b16
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-05-05 15:43:07 +0500

    #8kyu Find the Difference in Age between Oldest and Youngest Family Members

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -1241,3 +1241,13 @@ console.log(formatMoney(39)); */
 console.log(
   cannonsReady({ Mike: "aye", Joe: "aye", Johnson: "aye", Peter: "aye" })
 ); */
+
+// #8kyu Find the Difference in Age between Oldest and Youngest Family Members
+/* function differenceInAges(ages) {
+  return [
+    Math.min(...ages),
+    Math.max(...ages),
+    Math.max(...ages) - Math.min(...ages),
+  ];
+}
+console.log(differenceInAges([82, 15, 6, 38, 35])); */
