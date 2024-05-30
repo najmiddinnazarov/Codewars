19d46a6f0ece23c31b360805ee986dd6d3bae47b
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-05-30 15:49:02 +0500

    #8kyu Merge two sorted arrays into one

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -1273,3 +1273,9 @@ console.log(drawStairs(3)); */
   }
 }
 console.log(evil(2)); */
+
+// #8kyu Merge two sorted arrays into one
+/* function mergeArrays(arr1, arr2) {
+  return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b);
+}
+console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12])); */
