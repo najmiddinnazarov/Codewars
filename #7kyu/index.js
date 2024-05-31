d73eb4903ddf6a1faf89368e8a808d6dfa83a62d
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-05-31 19:20:36 +0500

    #7kyu Turkish Numbers, 0-99

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -3858,3 +3858,37 @@ console.log(alternateCase("Hello World")); */
   return Object.values(obj).includes(3) && Object.values(obj).includes(2);
 }
 console.log(checkThreeAndTwo(["a", "a", "a", "b", "b"])); */
+
+// #7kyu Turkish Numbers, 0-99
+/* const getTurkishNumber = (num) => {
+  const singleNumber = {
+    0: "sıfır",
+    1: "bir",
+    2: "iki",
+    3: "üç",
+    4: "dört",
+    5: "beş",
+    6: "altı",
+    7: "yedi",
+    8: "sekiz",
+    9: "dokuz",
+  };
+  const tensNumber = {
+    10: "on",
+    20: "yirmi",
+    30: "otuz",
+    40: "kırk",
+    50: "elli",
+    60: "altmış",
+    70: "yetmiş",
+    80: "seksen",
+    90: "doksan",
+  };
+  if (singleNumber[num] || tensNumber[num]) {
+    return singleNumber[num] || tensNumber[num];
+  } else {
+    num = num.toString();
+    return `${tensNumber[num[0] + 0]} ${singleNumber[num[1]]}`;
+  }
+};
+console.log(getTurkishNumber(20)); */
