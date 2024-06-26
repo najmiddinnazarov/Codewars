ba36845307d747a4bfe71d2f7d52927976cb9f98
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-06-26 11:36:10 +0500

    #7kyu No Loops 2 - You only need one

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -4113,3 +4113,9 @@ console.log(
     "Blitzen Claus",
   ])
 ); */
+
+// #7kyu No Loops 2 - You only need one
+/* function check(a, x) {
+  return a.includes(x);
+}
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45)); */
