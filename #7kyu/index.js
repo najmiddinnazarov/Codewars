3f3f83d9a2a48ee921b1bb5d10562f78fe9f3bf8
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-08-04 20:52:22 +0500

    #7kyu Find the Missing Number

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -4272,3 +4272,20 @@ console.log(tripleX("softx kitty, warm kitty, xxxxx")); */
 // #7kyu Truthy and Falsy
 /* const truthy = [1, 2, 3, 4, 5];
 const falsy = [NaN, 0, false, null, undefined]; */
+
+// #7kyu Find the Missing Number
+/* function missingNo(nums) {
+  const n = 100;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((acc, num) => acc + num, 0);
+  return expectedSum - actualSum;
+}
+const nums = [
+  9, 45, 53, 10, 100, 30, 85, 72, 69, 93, 98, 27, 73, 82, 91, 60, 5, 79, 88, 18,
+  71, 36, 44, 22, 89, 40, 59, 80, 81, 67, 25, 54, 13, 64, 56, 39, 48, 92, 84,
+  94, 87, 90, 77, 63, 32, 68, 37, 96, 23, 0, 95, 1, 52, 78, 6, 57, 50, 2, 46,
+  19, 76, 47, 14, 4, 3, 29, 17, 11, 21, 24, 74, 65, 12, 83, 28, 41, 66, 7, 58,
+  55, 51, 43, 97, 42, 86, 49, 31, 20, 75, 70, 34, 33, 38, 8, 15, 62, 35, 61, 99,
+  16,
+];
+console.log(missingNo(nums)); */
