abfe43734edaf1c55a5dbe89fc8fe3cc159fb8a4
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-08-24 11:39:31 +0500

    Grader

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -1397,3 +1397,19 @@ console.log(whoIsPaying("Melissa")); */
   return petal[nbPetals % 6];
 }
 console.log(howMuchILoveYou(6)); */
+
+// #8kyu Grader
+/* function grader(score) {
+  if (score < 0.6 || score > 1) {
+    return "F";
+  } else if (score >= 0.9) {
+    return "A";
+  } else if (score >= 0.8) {
+    return "B";
+  } else if (score >= 0.7) {
+    return "C";
+  } else if (score >= 0.6) {
+    return "D";
+  }
+}
+console.log(grader(0.7)); */
