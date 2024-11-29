b19ddeb6e8e5c1bbd6dad0eb108c18ce07172412
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-11-29 10:42:55 +0500

    We Have Liftoff

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -5537,3 +5537,9 @@ console.log(
     "what makes the desert beautiful, said the little prince is that somewhere it hides a well"
   )
 ); */
+
+// #7kyu We Have Liftoff
+/* function liftoff(instructions) {
+  return instructions.sort((a, b) => b - a).join(" ") + " " + "liftoff!";
+}
+console.log(liftoff([2, 8, 10, 9, 1, 3, 4, 7, 6, 5])); */
