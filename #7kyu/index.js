05995d461470850a6a9ab7d89f6070fc980f1b2d
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-12-06 10:58:18 +0500

    How sexy is your name?

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -5629,3 +5629,50 @@ console.log(alwaysOdd(1)); */
   return true;
 };
 console.log(comfortableWord("yams")); */
+
+// #7kyu How sexy is your name?
+/* function sexyName(name) {
+  name = name.toUpperCase();
+  const scores = {
+    A: 100,
+    B: 14,
+    C: 9,
+    D: 28,
+    E: 145,
+    F: 12,
+    G: 3,
+    H: 10,
+    I: 200,
+    J: 100,
+    K: 114,
+    L: 100,
+    M: 25,
+    N: 450,
+    O: 80,
+    P: 2,
+    Q: 12,
+    R: 400,
+    S: 113,
+    T: 405,
+    U: 11,
+    V: 10,
+    W: 10,
+    X: 3,
+    Y: 210,
+    Z: 23,
+  };
+  let score = 0;
+  for (let i = 0; i < name.length; i++) {
+    score += scores[name[i]] || 0;
+  }
+  if (score <= 60) {
+    return "NOT TOO SEXY";
+  } else if (61 <= score && score <= 300) {
+    return "PRETTY SEXY";
+  } else if (301 <= score && score <= 599) {
+    return "VERY SEXY";
+  } else if (score >= 600) {
+    return "THE ULTIMATE SEXIEST";
+  }
+}
+console.log(sexyName("GUV")); */
