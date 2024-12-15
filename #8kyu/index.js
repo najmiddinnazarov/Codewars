04f211ad0dab5749826ff429d13f40a4b8b6b7c9
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2024-12-15 15:12:49 +0500

    Student's Final Grade

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -1616,3 +1616,17 @@ console.log(
   return sum;
 }
 console.log(power(2, 2)); */
+
+// #8kyu Student's Final Grade
+/* function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+console.log(finalGrade(100, 12)); */
