299e6ed99ec2f89d9844fef601cb8b9ca050905d
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-03-06 11:49:48 +0500

    Number encrypting: cypher

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -6690,3 +6690,29 @@ console.log(
     10
   )
 ); */
+
+// #7kyu Number encrypting: cypher
+/* function cypher(string) {
+  const object = {
+    O: "0",
+    I: 1,
+    R: 2,
+    E: 3,
+    A: 4,
+    S: 5,
+    G: 6,
+    T: 7,
+    B: 8,
+    o: "0",
+    l: 1,
+    z: 2,
+    e: 3,
+    a: 4,
+    s: 5,
+    b: 6,
+    t: 7,
+    g: 9,
+  };
+  return string.replace(/[A-Za-z]/g, (x) => (object[x] ? object[x] : x));
+}
+console.log(cypher("Hello World")); */
