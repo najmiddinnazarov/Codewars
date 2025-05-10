471d277edf85ba1cef40d6fec9180813173fee8a
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-05-10 14:21:59 +0500

    Coding Meetup #13 - Higher-Order Functions Series - Is the meetup language-diverse?

--- #6kyu/index.js
+++ #6kyu/index.js
@@ -2118,3 +2118,74 @@ console.log(
     },
   ])
 ); */
+
+// #6kyu Coding Meetup #13 - Higher-Order Functions Series - Is the meetup language-diverse?
+/* function isLanguageDiverse(list) {
+  let js = 0;
+  let py = 0;
+  let rb = 0;
+  list.forEach((obj) => {
+    if (obj.language === "JavaScript") {
+      js++;
+    } else if (obj.language === "Python") {
+      py++;
+    } else if (obj.language === "Ruby") {
+      rb++;
+    }
+  });
+  const maxCount = Math.max(js, py, rb);
+  const minCount = Math.min(js, py, rb);
+  return maxCount <= 2 * minCount;
+}
+console.log(
+  isLanguageDiverse([
+    {
+      firstName: "Daniel",
+      lastName: "J.",
+      country: "Aruba",
+      continent: "Americas",
+      age: 42,
+      language: "Python",
+    },
+    {
+      firstName: "Kseniya",
+      lastName: "T.",
+      country: "Belarus",
+      continent: "Europe",
+      age: 22,
+      language: "Ruby",
+    },
+    {
+      firstName: "Sou",
+      lastName: "B.",
+      country: "Japan",
+      continent: "Asia",
+      age: 43,
+      language: "Ruby",
+    },
+    {
+      firstName: "Hanna",
+      lastName: "L.",
+      country: "Hungary",
+      continent: "Europe",
+      age: 95,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Jayden",
+      lastName: "P.",
+      country: "Jamaica",
+      continent: "Americas",
+      age: 18,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Joao",
+      lastName: "D.",
+      country: "Portugal",
+      continent: "Europe",
+      age: 25,
+      language: "JavaScript",
+    },
+  ])
+); */
