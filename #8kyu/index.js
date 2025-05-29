e1a3068e9bb58ee86cdac72bded901443a761717
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-05-29 19:06:46 +0500

    Grasshopper - Debug

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -1854,3 +1854,15 @@ console.log(cubeChecker(27, 3)); */
   return true;
 }
 console.log(isOpposite("ab", "AB")); */
+
+// #8kyu Grasshopper - Debug
+/* function weatherInfo(temp) {
+  var c = convertToCelsius(temp);
+  if (c < 0) return c + " is freezing temperature";
+  else return c + " is above freezing temperature";
+}
+function convertToCelsius(temperature) {
+  var celsius = (temperature - 32) * (5 / 9);
+  return celsius;
+}
+console.log(weatherInfo(50)); */
