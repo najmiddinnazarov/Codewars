083633c6bb5a54a310505a7cbfc35828263b996a
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-06-14 18:38:02 +0500

    Tip Calculator

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -2021,3 +2021,23 @@ console.log(chromosomeCheck("XY")); */
   };
 }
 console.log(wrap(343)); */
+
+// #8kyu Tip Calculator
+/* function calculateTip(amount, rating) {
+  rating = rating.toLowerCase();
+  switch (rating) {
+    case "terrible":
+      return 0;
+    case "poor":
+      return Math.ceil(amount * 0.05);
+    case "good":
+      return Math.ceil(amount * 0.1);
+    case "great":
+      return Math.ceil(amount * 0.15);
+    case "excellent":
+      return Math.ceil(amount * 0.2);
+    default:
+      return "Rating not recognised";
+  }
+}
+console.log(calculateTip(26.95, "good")); */
