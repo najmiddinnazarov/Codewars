af0a24cd2d1c5f6ab0b8e10ca25c84617451c2ca
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-06-17 14:00:18 +0500

    SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -2063,3 +2063,10 @@ console.log(apple(4)); */
     "coffee-script": "~1.6.3",
   },
 }; */
+
+// #8kyu SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes
+/* class Labrador {
+constructor(name, age, gender, master){this.name = name;this.age = age;this.gender = gender;this.species = "Labrador";this.legs = 4;this.size = "Large";this.master = master;this.loyal = true;}
+}
+var edward = new Labrador("Edward", 3, "Male", "Emma");
+console.log(edward); */
