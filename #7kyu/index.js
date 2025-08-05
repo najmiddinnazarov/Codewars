ec7d0dc7d0671eb6ce9bdcaa407774f343a042bb
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-08-05 06:19:16 +0500

    Scrabblemania

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -7785,3 +7785,44 @@ console.log(f()); */
   }
 }
 console.log(median([33, 99, 100, 30, 29, 50])); */
+
+// #7kyu Scrabblemania
+/* function wordscore(word) {
+  const object = {
+    a: 1,
+    b: 3,
+    c: 3,
+    d: 2,
+    e: 1,
+    f: 4,
+    g: 2,
+    h: 4,
+    i: 1,
+    j: 8,
+    k: 5,
+    l: 1,
+    m: 3,
+    n: 1,
+    o: 1,
+    p: 3,
+    q: 10,
+    r: 1,
+    s: 1,
+    t: 1,
+    u: 1,
+    v: 4,
+    w: 4,
+    x: 8,
+    y: 4,
+    z: 10,
+  };
+  let score = 0;
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i].toLowerCase();
+    if (object.hasOwnProperty(letter)) {
+      score += object[letter];
+    }
+  }
+  return word.length === 7 ? score * word.length + 50 : score * word.length;
+}
+console.log(wordscore("deceive")); */
