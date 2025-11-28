68b6d9cb07e89d9ad5cd7fd7dffc31003117a688
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2025-11-28 19:51:20 +0300

    Trimming a string

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -9288,3 +9288,19 @@ console.log(sumOfIntegersInString("Dogs are our best friends.")); */
 console.log(
   printerError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
 ); */
+
+// #7kyu Trimming a string
+/* function trim(str, size) {
+  if (str.length <= size) {
+    return str;
+  }
+  if (size > 3) {
+    let cutText = str.slice(0, size - 3);
+    return cutText + "...";
+  }
+  if (size <= 3) {
+    let cutText = str.slice(0, size);
+    return cutText + "...";
+  }
+}
+console.log(trim("Code Wars is pretty rad", 50)); */
