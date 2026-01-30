365dfcfdbec53f2c8f16724de558dd7dc7c65357
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2026-01-30 15:40:01 +0300

    How much water do I need?

--- #8kyu/index.js
+++ #8kyu/index.js
@@ -2279,3 +2279,16 @@ console.log(aliasGen("Mike", "Millington")); */
 /* function findStrangeValue() {
   return NaN;
 } */
+
+// #8kyu How much water do I need?
+/* function howMuchWater(water, load, clothes) {
+  if (clothes > 2 * load) {
+    return "Too much clothes";
+  }
+  if (clothes < load) {
+    return "Not enough clothes";
+  }
+  let result = water * Math.pow(1.1, clothes - load);
+  return Number(result.toFixed(2));
+}
+console.log(howMuchWater(5, 10, 14)); */
