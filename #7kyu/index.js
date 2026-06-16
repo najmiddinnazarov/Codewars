b3399f34cd955e609182ac6f677751411683a265
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2026-06-16 19:30:20 +0300

    Gematria for all

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -11985,3 +11985,43 @@ console.log(waterbombs("xxxxYxYx", 4)); */
   ];
 }
 console.log(evenAndOdd(126453)); */
+
+// #7kyu Gematria for all
+/* function gematria(str) {
+  const gematriaValues = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    k: 10,
+    l: 20,
+    m: 30,
+    n: 40,
+    o: 50,
+    p: 60,
+    q: 70,
+    r: 80,
+    s: 90,
+    t: 100,
+    u: 200,
+    x: 300,
+    y: 400,
+    z: 500,
+    j: 600,
+    v: 700,
+    w: 900,
+  };
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (/[A-Za-z]/.test(str[i])) {
+      sum += gematriaValues[str[i].toLowerCase()];
+    }
+  }
+  return sum;
+}
+console.log(gematria("Devil")); */
