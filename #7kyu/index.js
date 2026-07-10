21ebc08a5d0d3b10bcfbc7a4e4a96bef9336978e
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2026-07-10 21:43:02 +0300

    Battle of the characters (Medium)

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -12298,3 +12298,67 @@ console.log(theVar("d+g")); */
   return [simple, compound];
 }
 console.log(interest(100, 0.1, 1)); */
+
+// #7kyu Battle of the characters (Medium)
+/* function battle(x, y) {
+  const powers = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26,
+    a: 0.5,
+    b: 1,
+    c: 1.5,
+    d: 2,
+    e: 2.5,
+    f: 3,
+    g: 3.5,
+    h: 4,
+    i: 4.5,
+    j: 5,
+    k: 5.5,
+    l: 6,
+    m: 6.5,
+    n: 7,
+    o: 7.5,
+    p: 8,
+    q: 8.5,
+    r: 9,
+    s: 9.5,
+    t: 10,
+    u: 10.5,
+    v: 11,
+    w: 11.5,
+    x: 12,
+    y: 12.5,
+    z: 13,
+  };
+  let a = 0;
+  let b = 0;
+  for (const ch of x) a += powers[ch];
+  for (const ch of y) b += powers[ch];
+  return a === b ? "Tie!" : a > b ? x : y;
+}
+console.log(battle("One", "Tw")); */
