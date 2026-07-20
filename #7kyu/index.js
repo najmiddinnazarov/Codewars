691f627ea5441833be8f73211ba60359e3be2375
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2026-07-20 20:46:23 +0300

    Olympic Rings

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -12522,3 +12522,34 @@ console.log(solve("a")); */
   return count;
 }
 console.log(numCombo([2, 0, 0, 0, 1], 2)); */
+
+// #7kyu Olympic Rings
+/* function olympicRing(a) {
+  const rings = {
+    A: 1,
+    B: 2,
+    D: 1,
+    O: 1,
+    P: 1,
+    Q: 1,
+    R: 1,
+    a: 1,
+    b: 1,
+    d: 1,
+    e: 1,
+    g: 1,
+    o: 1,
+    p: 1,
+    q: 1,
+  };
+  let total = 0;
+  for (const ch of a) {
+    total += rings[ch] || 0;
+  }
+  const score = Math.floor(total / 2);
+  if (score <= 1) return "Not even a medal!";
+  if (score === 2) return "Bronze!";
+  if (score === 3) return "Silver!";
+  return "Gold!";
+}
+console.log(olympicRing("wHjMudLwtoPGocnJ")); */
