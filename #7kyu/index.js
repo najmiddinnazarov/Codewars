b42bdd6c29f8050877953963d4c8f66f9e8ac9af
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2026-09-18 12:11:21 +0300

    Supernatural

--- #7kyu/index.js
+++ #7kyu/index.js
@@ -13375,3 +13375,37 @@ console.log(sharePrice(100, [-50, 50])); */
   return intersection / union;
 }
 console.log(similarity([1, 2, 3], [1, 2, 3])); */
+
+// #7kyu Supernatural
+/* function bob(str) {
+  const drunken_doodling = {
+    werewolf: "Silver knife or bullet to the heart",
+    vampire: "Behead it with a machete",
+    wendigo: "Burn it to death",
+    shapeshifter: "Silver knife or bullet to the heart",
+    angel: "Use the angelic blade",
+    demon: "Use Ruby's knife, or some Jesus-juice",
+    ghost: "Salt and iron, and don't forget to burn the corpse",
+    dragon: "You have to find the excalibur for that",
+    djinn: "Stab it with silver knife dipped in a lamb's blood",
+    "pagan god": "It depends on which one it is",
+    leviathan: "Use some Borax, then kill Dick",
+    ghoul: "Behead it",
+    "jefferson starship": "Behead it with a silver blade",
+    reaper: "If it's nasty, you should gank who controls it",
+    rugaru: "Burn it alive",
+    skinwalker: "A silver bullet will do it",
+    phoenix: "Use the colt",
+    witch: "They are humans",
+    else: "I have friggin no idea yet",
+  };
+  return (
+    (drunken_doodling[str]
+      ? drunken_doodling[str]
+      : "I have friggin no idea yet") +
+    "," +
+    " " +
+    "idjits!"
+  );
+}
+console.log(bob("werewolf")); */
